c080a3096806697793012418842959e7734f1d02
SLovpache <SLovpache@croc.ru>
2021-02-10 12:04:20 +0300

    Доделывание игры

---
+++ src/store/modules/leaderboard/action-types.ts
@@ -0,0 +1,8 @@
+enum GameActionTypes {
+  UPDATE_FIELD_SIZE = 'UPDATE_FIELD_SIZE',
+  RESET_SCORE = 'RESET_SCORE',
+  OPEN_EMPTY_CELL = 'OPEN_EMPTY_CELL',
+  ADD_POINTS_FOR_BOMBS = 'ADD_POINTS_FOR_BOMBS'
+}
+
+export default GameActionTypes;
